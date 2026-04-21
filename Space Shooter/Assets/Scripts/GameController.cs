cf0a9a34ba45708defe196c91e50e1854bd52000
ethan <ethan@flightpaperstudio.com>
2026-04-20 22:13:28 -0500

    Added spawn timing

--- Space Shooter/Assets/Scripts/GameController.cs
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -15,21 +15,51 @@ public class GameController : MonoBehaviour
 	[SerializeField]
 	private int hazardsPerWave;
 
+	// The amount of time in seconds to wait before spawning the first wave
+	[SerializeField]
+	private float startWait;
+
+	// The amount of time in seconds to wait before spawning hazards
+	[SerializeField]
+	private float spawnWait;
+
+	// The amount of time in seconds to wait before spawning the next wave
+	[SerializeField]
+	private float waveWait;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-		for ( int i = 0; i < hazardsPerWave; i++ )
-		{
-			GameObject hazard = hazards [ Random.Range ( 0, hazards.Length ) ];
-			SpawnHazard ( hazard );
-		}
+		// Start spawning waves
+		StartCoroutine ( SpawnWaves ( ) );
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// SpawnWaves will continuously spawn waves of hazards until the game ends
+	private IEnumerator SpawnWaves()
+	{
+		// Wait before spawning the first wave
+		yield return new WaitForSeconds ( startWait );
+
+		// Continuously spawn waves
+		while(true)
+		{
+			// Spawn each hazard in the wave
+			for ( int i = 0; i < hazardsPerWave; i++ )
+			{
+				// Get a random hazard
+				GameObject hazard = hazards [ Random.Range ( 0, hazards.Length ) ];
+				
+				// Spawn the random hazard
+				SpawnHazard ( hazard );
+
+				// Wait before spawning the next hazard
+				yield return new WaitForSeconds ( spawnWait );
+			}
+
+			// Wait before spawning the next wave
+			yield return new WaitForSeconds ( waveWait );
+		}
+	}
 
 	// SpawnHazard will spawn a given hazard at a random location
 	private void SpawnHazard ( GameObject hazard )
